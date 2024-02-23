b9150a8bded9a2b5a2e3b5b80f318deede7db669
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2024-02-23 18:25:04 +0000

    update: first simple server created with node-js. upnext is routing

--- projects/01-simple-server/server.js
+++ projects/01-simple-server/server.js
@@ -1,1 +1,10 @@
-console.log("Starting here!")
+const http = require("http")
+
+const server = http.createServer((req, res) => {
+    res.end("<h1 style=\"text-align: center; margin-top: 30px; color: #555\">SIMPLE SERVER</h1>")
+})
+
+
+server.listen(5000, "localhost", () => {
+    console.log("Server is listening on port 5000.")
+})
