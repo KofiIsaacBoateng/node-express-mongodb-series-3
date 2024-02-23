c09c14208a197d95b845c005093cf2f7f6d7f915
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2024-02-23 19:09:29 +0000

    update: added a route for /api requests to respond with json data. BASIC!

--- projects/01-simple-server/server.js
+++ projects/01-simple-server/server.js
@@ -1,34 +1,51 @@
 const http = require("http")
+const fs = require('fs')
+
+
+const data = fs.readFileSync(`${__dirname}/data.json`)
+const dataObject = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
     const pathName = req.url
     let message
     let statusCode
+    let contentType
 
     switch(pathName){
         case "/":
             message = "See Everything In One View"
+            contentType="text/html"
             statusCode = 200
             break;
 
         case "/overview":
             message = "See Everything In One View"
+            contentType="text/html"
             statusCode = 200
             break;
 
         case "/products":
             message = "What would you want to buy? :)"
+            contentType="text/html"
             statusCode = 200
             break;
+
+        case "/api":
+            statusCode = 200
+            contentType = "application/json"
+            console.log(dataObject)
         default:
             message = "Oops Page Not Found!"
             statusCode = 404;
             break    
     }
     res.writeHead(statusCode, {
-        "Content-type": "text/html"
+        "Content-type": contentType
     })
-    res.end(`<h1 style="text-align: center; margin-top: 30px; color: #555; text-transform: uppercase;">${message}</h1>`)
+
+    pathName === "/api" ? 
+        res.end(data) :
+        res.end(`<h1 style="text-align: center; margin-top: 30px; color: #555; text-transform: uppercase;">${message}</h1>`)
 })
 
 
