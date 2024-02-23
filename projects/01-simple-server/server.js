23b28af38eef0d6eb05a2ec7c7570bec5bdeafc6
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2024-02-23 18:45:29 +0000

    update: still module 1 with basic routing

--- projects/01-simple-server/server.js
+++ projects/01-simple-server/server.js
@@ -1,7 +1,34 @@
 const http = require("http")
 
 const server = http.createServer((req, res) => {
-    res.end("<h1 style=\"text-align: center; margin-top: 30px; color: #555\">SIMPLE SERVER</h1>")
+    const pathName = req.url
+    let message
+    let statusCode
+
+    switch(pathName){
+        case "/":
+            message = "See Everything In One View"
+            statusCode = 200
+            break;
+
+        case "/overview":
+            message = "See Everything In One View"
+            statusCode = 200
+            break;
+
+        case "/products":
+            message = "What would you want to buy? :)"
+            statusCode = 200
+            break;
+        default:
+            message = "Oops Page Not Found!"
+            statusCode = 404;
+            break    
+    }
+    res.writeHead(statusCode, {
+        "Content-type": "text/html"
+    })
+    res.end(`<h1 style="text-align: center; margin-top: 30px; color: #555; text-transform: uppercase;">${message}</h1>`)
 })
 
 
