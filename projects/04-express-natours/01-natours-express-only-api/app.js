1f33c811dca20e8e8b6738621fccea311d1fd95b
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2024-02-26 16:45:34 +0000

    update: separated app from server, all express related content shall be found in the app module while server related components shall be located in the server module

---
+++ projects/04-express-natours/01-natours-express-only-api/app.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const fs = require("fs")
+const path = require("path")
+// routes
+const userRoutes = require("./routes/users")
+const tourRoutes = require("./routes/tours")
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+
+/** middlewares */
+app.use(express.json())
+
+/*** routes */
+app.use("/api/v1/users", userRoutes)
+app.use("/api/v1/tours", tourRoutes)
+
+
+module.exports = app
--- projects/04-express-natours/01-natours-express-only-api/server.js
+++ projects/04-express-natours/01-natours-express-only-api/server.js
@@ -1,21 +1,6 @@
-const express = require("express")
-const fs = require("fs")
-const path = require("path")
-// routes
-const userRoutes = require("./routes/users")
-const tourRoutes = require("./routes/tours")
+const app = require("./app")
+const port = process.env.PORT || 3000
 
-const app = express()
-const PORT = process.env.PORT || 3000
-
-
-
-/** middlewares */
-app.use(express.json())
-
-/*** routes */
-app.use("/api/v1/users", userRoutes)
-app.use("/api/v1/tours", tourRoutes)
-
-
-app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`))
+app.listen(port, () => {
+    console.log(`Server is listening on port: ${port}`)
+})
