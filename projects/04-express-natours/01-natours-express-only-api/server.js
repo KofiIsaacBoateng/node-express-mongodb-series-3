78774facbaf63cbbaaa1e317d73267b1fd711d59
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2024-02-28 03:43:02 +0000

    update: configured environment variables

--- projects/04-express-natours/01-natours-express-only-api/server.js
+++ projects/04-express-natours/01-natours-express-only-api/server.js
@@ -1,5 +1,6 @@
+require("dotenv").config()
 const app = require("./app")
-const port = process.env.PORT || 3500
+const port = process.env.PORT
 
 app.listen(port, () => {
     console.log(`Server is listening on port: ${port}`)
