760b0c2cb495930a2b02e886ed83e09b249e2b84
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2024-03-02 11:09:45 +0000

    update: created a basic tour model and saved a few ddocuments in the database

--- projects/04-express-natours/02-natours-express-mongodb-api/server.js
+++ projects/04-express-natours/02-natours-express-mongodb-api/server.js
@@ -1,10 +1,9 @@
 require("dotenv").config();
 const app = require("./app");
-const mongoose = require("mongoose");
+const { connect, Schema, model } = require("mongoose");
 const port = process.env.PORT;
 
-mongoose
-  .connect(process.env.MONGO_URI)
+connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Database connected!");
     app.listen(port, () => {
@@ -12,3 +11,33 @@ mongoose
     });
   })
   .catch((err) => console.log(err));
+
+const tourSchema = Schema({
+  name: {
+    type: String,
+    required: [true, "Name field cannot be empty"],
+    unique: [true, "Name is taken!"],
+  },
+
+  rating: {
+    type: Number,
+    default: 0.0,
+  },
+  price: {
+    type: Number,
+    required: [true, "Price field cannot be empty"],
+  },
+});
+
+const Tour = model("Tour", tourSchema);
+
+const testTour = new Tour({
+  name: "The Fat Slayer",
+  rating: 4.9,
+  price: 799,
+});
+
+testTour
+  .save()
+  .then((doc) => console.log(doc))
+  .catch((err) => console.log(err.code));
