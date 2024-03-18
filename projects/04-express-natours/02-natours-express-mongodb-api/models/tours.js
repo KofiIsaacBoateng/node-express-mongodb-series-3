b2eddd57fe55ff8048f200ed581bc81e3a1e0d0d
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2024-03-18 21:36:36 +0000

    update: tours model made a little bit more robust. Also added a script to bulk upload and bult delete data from the database

--- projects/04-express-natours/02-natours-express-mongodb-api/models/tours.js
+++ projects/04-express-natours/02-natours-express-mongodb-api/models/tours.js
@@ -2,21 +2,70 @@ const { model, Schema, Types } = require("mongoose");
 
 const tourSchema = new Schema({
   _id: Types.ObjectId,
-
   name: {
     type: String,
     required: [true, "Name field cannot be empty"],
     unique: [true, "Name is taken!"],
   },
-
-  rating: {
-    type: Number,
-    default: 0.0,
+  duration: {
+    type: String,
+    required: [true, "Please specify the tour duration"],
   },
+
+  maxGroupSize: {
+    type: Number,
+    required: [
+      true,
+      "Must specify the maximum number of people that can be in attendance",
+    ],
+  },
+
+  difficulty: {
+    type: String,
+    required: [true, "Please state the level of difficulty"],
+  },
+
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+
   price: {
     type: Number,
     required: [true, "Price field cannot be empty"],
   },
+
+  priceDiscount: Number,
+
+  description: {
+    type: String,
+    trim: true,
+  },
+
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, "Summary can't be missing"],
+  },
+
+  imageCover: {
+    type: String,
+    required: [true, "cover image is missing"],
+  },
+
+  images: [String],
+
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+
+  startDates: [Date],
 });
 
 module.exports = model("Tour", tourSchema);
---
+++ projects/04-express-natours/dev-data/data/upload-dev-data.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
+const MONGO_URI = process.env.MONGO_URI;
+const Tours = require("../../02-natours-express-mongodb-api/models/tours");
+
+const bulkToursData = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "tours-simple.json"), "utf-8")
+);
+
+mongoose
+  .connect(MONGO_URI)
+  .then((con) => {
+    console.log("database connection successful");
+  })
+  .catch((error) => console.error("db connection error: ", error));
+
+const uploadBulkData = async () => {
+  try {
+    const tours = await Tours.insertMany(bulkToursData);
+    console.log("uploaded data successfully!");
+    process.exit();
+  } catch (error) {
+    console.log("upload error: ", error);
+  }
+};
+
+const deleteBulkData = async () => {
+  try {
+    const tours = await Tours.deleteMany();
+    console.log("cleared data successfully!");
+    process.exit();
+  } catch (error) {
+    console.log("upload error: ", error);
+  }
+};
+
+if (process.argv[2] === "--import") {
+  uploadBulkData();
+} else if (process.argv[2] === "--clear") {
+  deleteBulkData();
+}
