501f9c1df95e9d2970cd57890103b8ba1d13235b
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2024-03-31 19:20:42 +0000

    update: update-me and delete-me and get all users

--- projects/04-express-natours/03-natours-express-mongodb-authentication-and-authorization/controllers/users.js
+++ projects/04-express-natours/03-natours-express-mongodb-authentication-and-authorization/controllers/users.js
@@ -1,39 +1,79 @@
-const path = require("path")
-const fs = require("fs")
-const users = path.join(__dirname, "..", "dev-data", "data", "users.json") // robust data with a lot more fields
+const path = require("path");
+const fs = require("fs");
+const User = require("../models/user");
+const asyncWrapper = require("../utils/asyncWrapper");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors");
+const users = path.join(__dirname, "..", "dev-data", "data", "users.json"); // robust data with a lot more fields
 
+/*** GET ALL USERS */
+module.exports.getAllUsers = asyncWrapper(async (req, res) => {
+  const users = await User.find({});
 
-module.exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-        success: false,
-        msg: "Sorry! route not defined yet"
-    })
-}
+  res.status(StatusCodes.OK).json({
+    success: true,
+    results: users.length,
+    data: users,
+  });
+});
 
+/**** UPDATE ME */
+module.exports.updateMe = asyncWrapper(async (req, res) => {
+  const { password, passwordConfirm, ...body } = req.body;
+
+  if (password || passwordConfirm) {
+    throw new BadRequestError(
+      "Password cannot be reset over here. Please checkout [/update-password] route"
+    );
+  }
+
+  const user = await User.findByIdAndUpdate(req.user._id, body, {
+    runValidators: true,
+    new: true,
+  });
+
+  res.status(StatusCodes.OK).json({
+    success: true,
+    data: user,
+  });
+});
+
+module.exports.deleteMe = asyncWrapper(async (req, res) => {
+  await User.findByIdAndDelete(req.user._id);
+
+  res.status(StatusCodes.NO_CONTENT).json({
+    success: true,
+    message: "user deleted successfully",
+  });
+});
+
+/*** DELETE ME */
+
+/**** MAIN */
 module.exports.getUser = (req, res) => {
-    res.status(500).json({
-        success: false,
-        msg: "Sorry! route not defined yet"
-    })
-}
+  res.status(500).json({
+    success: false,
+    msg: "Sorry! route not defined yet",
+  });
+};
 
 module.exports.createUser = (req, res) => {
-    res.status(500).json({
-        success: false,
-        msg: "Sorry! route not defined yet"
-    })
-}
+  res.status(500).json({
+    success: false,
+    msg: "Sorry! route not defined yet",
+  });
+};
 
 module.exports.updateUser = (req, res) => {
-    res.status(500).json({
-        success: false,
-        msg: "Sorry! route not defined yet"
-    })
-}
+  res.status(500).json({
+    success: false,
+    msg: "Sorry! route not defined yet",
+  });
+};
 
 module.exports.deleteUser = (req, res) => {
-    res.status(500).json({
-        success: false,
-        msg: "Sorry! route not defined yet"
-    })
-}
+  res.status(500).json({
+    success: false,
+    msg: "Sorry! route not defined yet",
+  });
+};
--- projects/04-express-natours/03-natours-express-mongodb-authentication-and-authorization/routes/users.js
+++ projects/04-express-natours/03-natours-express-mongodb-authentication-and-authorization/routes/users.js
@@ -6,12 +6,19 @@ const {
   createUser,
   updateUser,
   deleteUser,
+  deleteMe,
+  updateMe,
 } = require("../controllers/users");
 const { routeProtector, updatePassword } = require("../controllers/auth");
 
 // update password
 router.route("/update-password").post(routeProtector, updatePassword);
 
+// update and delete me
+router.route("/update-me").patch(routeProtector, updateMe);
+router.route("/delete-me").delete(routeProtector, deleteMe);
+
+// main
 router.route("/").get(getAllUsers).post(createUser);
 
 router.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
