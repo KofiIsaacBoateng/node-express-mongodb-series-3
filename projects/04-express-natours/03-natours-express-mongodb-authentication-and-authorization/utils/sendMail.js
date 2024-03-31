a9aea431e87456f7a58514983a6b13c977b9e30c
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2024-03-31 17:58:43 +0000

    fix: email error fixed

--- projects/04-express-natours/03-natours-express-mongodb-authentication-and-authorization/utils/sendMail.js
+++ projects/04-express-natours/03-natours-express-mongodb-authentication-and-authorization/utils/sendMail.js
@@ -3,11 +3,11 @@ const nodemailer = require("nodemailer");
 const sendMail = async ({ message, subject, email }) => {
   /**** create transporter */
   const transporter = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT * 1,
     auth: {
-      user: "16bb58f5404864",
-      pass: "6ca25b14b168d0",
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
     },
   });
 
